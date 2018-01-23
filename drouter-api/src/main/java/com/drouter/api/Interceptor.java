57c54c2b98cbd0c1392929e109831c428f2a6a7c
DarrenHui <z14726932514@gmail.com>
2018-01-23 18:35:49 +0800

    检测 Action 名称冲突的问题

--- drouter-api/src/main/java/com/drouter/api/Interceptor.java
+++ drouter-api/src/main/java/com/drouter/api/Interceptor.java
@@ -1,10 +1,11 @@
 package com.drouter.api;
 
 /**
- * description:
+ * description: 拦截器
  * author: Darren on 2018/1/22 11:59
  * email: 240336124@qq.com
  * version: 1.0
  */
-public class Interceptor {
+public interface Interceptor {
+    
 }
