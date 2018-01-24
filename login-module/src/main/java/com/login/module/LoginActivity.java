9a6a2840eee96adccd4e42f2b0c3a7c340c51eb9
DarrenHui <z14726932514@gmail.com>
2018-01-24 10:48:32 +0800

    完善拦截器功能

--- login-module/src/main/java/com/login/module/LoginActivity.java
+++ login-module/src/main/java/com/login/module/LoginActivity.java
@@ -3,7 +3,13 @@ package com.login.module;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import com.drouter.api.core.DRouter;
+import com.drouter.api.result.ActionCallback;
+import com.drouter.api.result.RouterResult;
 
 /**
  * description:
@@ -19,5 +25,23 @@ public class LoginActivity extends AppCompatActivity {
 
         String key = getIntent().getStringExtra("key");
         Toast.makeText(this, key, Toast.LENGTH_LONG).show();
+    }
+
+    public void click(View view){
+        DRouter.getInstance()
+                .action("circlemodule/test")
+                .context(this)
+                .param("key", "value")
+                .invokeAction(new ActionCallback() {
+                    @Override
+                    public void onInterrupt() {
+                        Log.e("TAG", "被拦截了");
+                    }
+
+                    @Override
+                    public void onResult(RouterResult result) {
+                        Log.e("TAG", "result = " + result.toString());
+                    }
+                });
     }
 }
