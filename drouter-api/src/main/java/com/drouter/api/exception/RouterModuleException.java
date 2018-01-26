822fe8b0c5a0245bcd9aa4a41edeab299443ae70
DarrenHui <z14726932514@gmail.com>
2018-01-26 14:02:08 +0800

    完善拦截功能

--- drouter-api/src/main/java/com/drouter/api/exception/RouterModuleException.java
+++ drouter-api/src/main/java/com/drouter/api/exception/RouterModuleException.java
@@ -1,7 +1,7 @@
 package com.drouter.api.exception;
 
 /**
- * description: Router ActionWrapper Exception
+ * description: Router Module Exception
  * author: Darren on 2018/1/22 10:44
  * email: 240336124@qq.com
  * version: 1.0
